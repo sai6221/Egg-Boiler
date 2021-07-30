e120cbe7cc098467b49ba88c71f6bfbb80f06d4a
Sai Reddy <sai6221@gmail.com>
2021-07-30 16:38:58 +0530

    Initial Commit

---
+++ Egg Boiler/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Egg Boiler
+//
+//  Created by Sai Reddy on 30/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
